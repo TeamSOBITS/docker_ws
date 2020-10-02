4938ebf2e47bf883ed1783ff910b37a70b52c669
KoutaOhishi <bigbasketball568@yahoo.co.jp>
2020-10-02 22:47:23 +0900

    fix run.sh to use same volume with host pc

--- container/ros_kinetic_basic_ws/Dockerfiles/cpu/run.sh
+++ container/ros_kinetic_basic_ws/Dockerfiles/cpu/run.sh
@@ -3,6 +3,7 @@ docker run \
     -p 6080:80 \
     --device /dev/dri:/dev/dri \
     --device /dev/video0:/dev/video0:mwr \
+    -v ~/docker_ws/container/ros_kinetic_basic_ws/src:/home/sobits/catkin_ws/src \
     --shm-size=512m \
     --name ros_kinetic_basic_ws \
     --privileged \

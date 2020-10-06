32bbc58239fc85f7940caa388065c9e6cb304711
KoutaOhishi <bigbasketball568@yahoo.co.jp>
2020-10-06 18:03:12 +0900

    add comment to run shell file

--- container/rc2020jp/sim/final_ros/git_clone_ros_packages.sh
+++ container/rc2020jp/sim/final_ros/git_clone_ros_packages.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-sudo rm -rf ~/docker_ws/container/rc2020jp/sim/final_ros/src/
-mkdir  ~/docker_ws/container/rc2020jp/sim/final_ros/src/
-cd ~/docker_ws/container/rc2020jp/sim/final_ros/src/
+#実行時は、"sh <file name>"ではなく"bash <file name>"で実行してください。
+
+sudo rm -rf ~/docker_ws/container/rc2020jp/sim/final_ros/src
+mkdir  ~/docker_ws/container/rc2020jp/sim/final_ros/src
+cd ~/docker_ws/container/rc2020jp/sim/final_ros/src
+
 
 #git cloneしたいTeamSOBITSのROSパッケージを記述
 ros_packages=( \
     "display_text" \
     "ssd_node" \
-    "avatars_uncertainty_recognizer"
+    "avatars_uncertainty_recognizer" \
 )
 
 for ((i = 0; i < ${#ros_packages[@]}; i++)) {

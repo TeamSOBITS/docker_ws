b86541d7b88ac550bd9986911681de9a51ad7929
KoutaOhishi <bigbasketball568@yahoo.co.jp>
2020-10-07 10:43:47 +0900

    fix build.sh

--- container/rc2020jp/sim/final_ros/build.sh
+++ container/rc2020jp/sim/final_ros/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd ~/docker_ws/container/rc2020jp/sim/final_ros
-sh git_clone_ros_packages.sh
+bash git_clone_ros_packages.sh
 
 docker build \
     --tag sobits/rc2020jp_sim_final_ros \

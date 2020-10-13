122b05a5960fb1916b7d12e1fe32db9c7fc6762f
KoutaOhishi <bigbasketball568@yahoo.co.jp>
2020-10-13 11:44:56 +0900

    rm files

--- container/rc2020jp/sim/final_ros/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd ~/docker_ws/container/rc2020jp/sim/final_ros
-bash git_clone_ros_packages.sh
-
-docker build \
-    --tag sobits/rc2020jp_sim_final_ros \
-    --network host \
-    .

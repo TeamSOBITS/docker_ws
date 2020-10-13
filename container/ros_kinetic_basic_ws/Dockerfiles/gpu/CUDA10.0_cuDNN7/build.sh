e5ca03016ac99f87d8a16c5ad5d73205cfc78cbb
KoutaOhishi <bigbasketball568@yahoo.co.jp>
2020-10-13 11:06:26 +0900

    fix run.sh & build.sh

--- container/ros_kinetic_basic_ws/Dockerfiles/gpu/CUDA10.0_cuDNN7/build.sh
+++ container/ros_kinetic_basic_ws/Dockerfiles/gpu/CUDA10.0_cuDNN7/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-cd ~/docker_ws/container/ros_kinetic_basic_ws/Dockerfiles/gpu/tmp/
+cd ~/docker_ws/container/ros_kinetic_basic_ws/Dockerfiles/gpu/CUDA10.0_cuDNN7/
 
 docker build \
-    --tag sobits/ros_kinetic_basic_ws_gpu_tmp \
+    --tag sobits/ros_kinetic_basic_ws_gpu_CUDA10.0_cuDNN7 \
     --network host \
     .

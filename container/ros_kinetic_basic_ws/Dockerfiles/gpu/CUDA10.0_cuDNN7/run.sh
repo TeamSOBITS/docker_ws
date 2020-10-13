35d3f38b6840e44219b599e82fdd39c52bf3222c
KoutaOhishi <bigbasketball568@yahoo.co.jp>
2020-10-13 11:04:59 +0900

    fix run.sh & build.sh

--- container/ros_kinetic_basic_ws/Dockerfiles/gpu/CUDA10.0_cuDNN7/run.sh
+++ container/ros_kinetic_basic_ws/Dockerfiles/gpu/CUDA10.0_cuDNN7/run.sh
@@ -4,8 +4,10 @@ docker run \
     --gpus all \
     --device /dev/dri:/dev/dri \
     --device /dev/video0:/dev/video0:mwr \
-    -v ~/docker_ws/container/ros_kinetic_basic_ws/src:/home/sobits/catkin_ws/src \
+    --mount type=bind,src=$(pwd)/src/,dst=/home/sobits/catkin_ws/src/,bind-propagation=shared \
+    -e LOCAL_UID=$(id -u $USER) \
+    -e LOCAL_GID=$(id -g $USER) \
     --shm-size=512m \
-    --name ros_kinetic_basic_ws_gpu_tmp \
+    --name ros_kinetic_basic_ws_gpu_CUDA10.0_cuDNN7 \
     --privileged \
-    sobits/ros_kinetic_basic_ws_gpu_tmp
+    sobits/ros_kinetic_basic_ws_gpu_CUDA10.0_cuDNN7

4c4c099f7656a0175c4e8053373a5d04edb42eaf
Kouta_Ohishi <bigbasketball568@yahoo.co.jp>
2020-11-10 17:28:45 +0900

    update ros_entrypoint.sh

--- container/ros_kinetic_basic_ws/Dockerfiles/CUI/cpu/ros_entrypoint.sh
+++ container/ros_kinetic_basic_ws/Dockerfiles/CUI/cpu/ros_entrypoint.sh
@@ -3,7 +3,9 @@ set -e
 
 # setup ros environment
 source "/opt/ros/$ROS_DISTRO/setup.bash"
+source "/home/sobits/catkin_ws/deve/setup.bash"
 
 # load bashrc
 source "/home/sobits/.bashrc"
+
 exec "$@"

4d653289a18300e3d98bfd91df9cec12640d5bc6
Kouta_Ohishi <bigbasketball568@yahoo.co.jp>
2020-11-13 14:36:49 +0900

    update: build-tag

--- container/ros_kinetic_basic_ws/Dockerfiles/gpu/CUDA9.0_cuDNN7.6/build.sh
+++ container/ros_kinetic_basic_ws/Dockerfiles/gpu/CUDA9.0_cuDNN7.6/build.sh
@@ -2,7 +2,7 @@
 cd ~/docker_ws/container/ros_kinetic_basic_ws/Dockerfiles/gpu/CUDA9.0_cuDNN7.6/
 
 docker build \
-    --tag sobits/ros_kinetic_basic_ws_cuda9.0_cudnn7.6 \
+    --tag sobits/ros_kinetic_cuda9.0_cudnn7.6 \
     --network host \
     --build-arg IMAGE_NAME="sobits/ros_kinetic_cuda9.0_cudnn7.6" \
     .

ffec2f65aeb1e1696a008935d0a70ad23cbf2612
Kouta_Ohishi <bigbasketball568@yahoo.co.jp>
2020-11-13 16:40:58 +0900

    remove rc2020jp

--- container/rc2020jp/sim/final_ros/Dockerfiles/cpu/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd ~/docker_ws/container/rc2020jp/sim/final_ros
-bash git_clone_ros_packages.sh
-
-cd ~/docker_ws/container/rc2020jp/sim/final_ros/Dockerfiles/cpu/
-docker build \
-    --tag sobits/rc2020jp_sim_final_ros_cpu \
-    --network host \
-    .

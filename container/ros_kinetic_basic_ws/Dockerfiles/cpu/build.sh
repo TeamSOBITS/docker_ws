e9135d48b72b81dc7839e61160fa5badece096c3
Kouta_Ohishi <bigbasketball568@yahoo.co.jp>
2020-12-04 00:16:09 +0900

    マルチステージビルドに対応

--- container/ros_kinetic_basic_ws/Dockerfiles/cpu/build.sh
+++ container/ros_kinetic_basic_ws/Dockerfiles/cpu/build.sh
@@ -4,5 +4,4 @@ cd ~/docker_ws/container/ros_kinetic_basic_ws/Dockerfiles/cpu
 docker build \
     --tag sobits/ros_kinetic_basic_ws \
     --network host \
-    --build-arg OAUTH="CVbC1YVoCUN6x-uAQK61" \
     .

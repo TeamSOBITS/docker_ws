076e27bbf701cb8589869f3debe413467fe7fa24
em-admin <admin@em>
2024-05-04 15:21:19 +0900

    [UPD] install method

--- setup_sh/install_nvidia_docker.sh
+++ setup_sh/install_nvidia_docker.sh
@@ -4,20 +4,20 @@
 echo "╔══╣ Install: NVIDIA Container Toolkit (STARTING) ╠══╗"
 
 
-# Setup the package repository and the GPG key:
-distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
-      && curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
-      && curl -s -L https://nvidia.github.io/libnvidia-container/$distribution/libnvidia-container.list | \
-            sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
-            sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+# Configure the production repository:
+curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+  && curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
+    sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+    sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
 
 
-# Install the nvidia-docker2 package (and dependencies)
+
+# Install the NVIDIA Container Toolkit packages
 sudo apt-get update
-sudo apt-get install -y nvidia-docker2
+sudo apt-get install -y nvidia-container-toolkit
 
 
-# Configure the Docker daemon to recognize the NVIDIA Container Runtime
+# Configure the container runtime by using the nvidia-ctk command
 sudo nvidia-ctk runtime configure --runtime=docker
 
 

d4ac283d15806cde4dfa5cf3590509173fc6d9e9
MrKeith99 <kvalentincardenas@hotmail.com>
2024-12-05 12:26:25 +0900

    [INIT] cuda 12.5.1

---
+++ container/base_2204_ws/Dockerfiles/gpu/CUDA12.5.1_cuDNN9.2/run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+DIR=$(pwd)
+str=`echo ${DIR} | awk -F "/" '{ print $(NF - 3) }'`
+
+cd $(pwd)/../../../
+
+xhost +local:${USER}
+
+docker run -it \
+    --gpus all \
+    --env CONTAINER_NAME=${str} \
+    --env DISPLAY=${DISPLAY} \
+    --device /dev/snd \
+    --env ALSA_CARD=sofhdadsp \
+    --volume /etc/udev/rules.d/:/etc/udev/rules.d/ \
+    --volume $(pwd)/src/:/home/sobits/colcon_ws/src/ \
+    --shm-size=1g \
+    --net host \
+    --name ${str} \
+    --privileged \
+    --user sobits \
+    sobits/${str} \
+    /bin/bash

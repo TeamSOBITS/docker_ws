1d99f548f12b63e3431638a21f4e85dde1e482cb
MrKeith99 <kvalentincardenas@hotmail.com>
2024-12-05 22:18:30 +0900

    [FIX] audio

--- container/base_2204_ws/Dockerfiles/cpu/run.sh
+++ container/base_2204_ws/Dockerfiles/cpu/run.sh
@@ -8,32 +8,16 @@ cd $(pwd)/../../
 xhost +local:${USER}
 
 docker run -it \
-    -v /etc/udev/rules.d/:/etc/udev/rules.d/ \
-    -v $(pwd)/src/:/home/sobits/colcon_ws/src/ \
     --env CONTAINER_NAME=${str} \
+    --env DISPLAY=${DISPLAY} \
+    --device /dev/snd \
+    --env ALSA_CARD=sofhdadsp \
+    --volume /etc/udev/rules.d/:/etc/udev/rules.d/ \
+    --volume $(pwd)/src/:/home/sobits/colcon_ws/src/ \
     --shm-size=1g \
     --net host \
     --name ${str} \
     --privileged \
     --user sobits \
     sobits/${str} \
-    /bin/bash 
-
-    # -v /run/user/1000/pipewire-0:/tmp/pipewire-0 \
-    # -e PIPEWIRE_RUNTIME_DIR=/tmp \
-    # --device /dev/snd/ \
-    # --env LOCAL_UID=$(id -u ${USER}) \
-    # --env LOCAL_GID=$(id -g ${USER}) \
-    # --device /dev/:/dev/ \
-    # --device=/dev/video0 \
-    # -v /var/run/dbus/system_bus_socket:/var/run/dbus/system_bus_socket \
-    # -v /etc/localtime:/etc/localtime \
-    # -v /tmp/.X11-unix:/tmp/.X11-unix \
-    # -v /tmp/pulseaudio.socket:/tmp/pulseaudio.socket \
-    # -v /tmp/pulseaudio.client.conf:/tmp/pulseaudio.client.conf \
-    # -v /dev/input/:/dev/input/ \
-    # -v /dev/snd/:/dev/snd/ \
-    # --env PULSE_SERVER=unix:/tmp/pulseaudio.socket \
-    # --env PULSE_COOKIE=/tmp/pulseaudio.cookie \
-    # --env DISPLAY=${DISPLAY} \
-    # --env QT_X11_NO_MITSHM=1 \
+    /bin/bash

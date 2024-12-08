9b1c87361c25429b029d6d308c568f33165f7872
MrKeith99 <kvalentincardenas@hotmail.com>
2024-12-08 13:28:29 +0900

    [FIX] audio when running script

--- container/base_2204_ws/Dockerfiles/gpu/CUDA12.2.2_cuDNN8.9/run.sh
+++ container/base_2204_ws/Dockerfiles/gpu/CUDA12.2.2_cuDNN8.9/run.sh
@@ -9,9 +9,13 @@ xhost +local:${USER}
 
 docker run -it \
     --gpus all \
-    -v /etc/udev/rules.d/:/etc/udev/rules.d/ \
-    -v $(pwd)/src/:/home/sobits/colcon_ws/src/ \
+    --device /dev/snd \
     --env CONTAINER_NAME=${str} \
+    --env DISPLAY=${DISPLAY} \
+    --env PULSE_SERVER=unix:/run/user/$(id -u)/pulse/native \
+    --volume /run/user/$(id -u)/pulse:/run/user/$(id -u)/pulse \
+    --volume /etc/udev/rules.d/:/etc/udev/rules.d/ \
+    --volume $(pwd)/src/:/home/sobits/colcon_ws/src/ \
     --shm-size=1g \
     --net host \
     --name ${str} \
@@ -19,13 +23,3 @@ docker run -it \
     --user sobits \
     sobits/${str} \
     /bin/bash
-
-
-    # --mount type=bind,src=/var/run/dbus/system_bus_socket,dst=/var/run/dbus/system_bus_socket,bind-propagation=shared \
-    # --mount type=bind,src=/etc/localtime,dst=/etc/localtime,bind-propagation=shared \
-    # --mount type=bind,src=/tmp/.X11-unix,dst=/tmp/.X11-unix,bind-propagation=shared \
-    # --mount type=bind,src=/tmp/pulseaudio.socket,dst=/tmp/pulseaudio.socket,bind-propagation=shared \
-    # --mount type=bind,src=/tmp/pulseaudio.client.conf,dst=/tmp/pulseaudio.client.conf,bind-propagation=shared \
-    # --mount type=bind,src=/dev/input/,dst=/dev/input/,bind-propagation=shared \
-    # --mount type=bind,src=/dev/snd/,dst=/dev/snd/,bind-propagation=shared \
-    # --mount type=bind,src=$(pwd)/src/,dst=/home/sobits/catkin_ws/src/,bind-propagation=shared \

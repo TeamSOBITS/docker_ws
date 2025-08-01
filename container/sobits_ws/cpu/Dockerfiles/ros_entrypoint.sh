e94e3f45a5699fba21ea9090aafa464d357ed060
Suzuki Koshiro <ksuzuki9541@gmail.com>
2025-08-02 03:04:55 +0900

    [DEL] ros_entrypoint.sh

--- container/sobits_ws/cpu/Dockerfiles/ros_entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-# setup ros2 environment
-find $HOMEPATH/colcon_ws/src \( -name "*.py" -o -name "*.sh" \) -exec chmod 755 {} +
-source "/opt/ros/${ROS_DISTRO}/setup.bash"
-exec "$@"

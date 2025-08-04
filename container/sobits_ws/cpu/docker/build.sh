d91a1b32809559ad2a222eb4b93e4462d0b21785
Suzuki Koshiro <ksuzuki9541@gmail.com>
2025-08-05 02:15:16 +0900

    [FIX] UID and GID environment variables

--- container/sobits_ws/cpu/docker/build.sh
+++ container/sobits_ws/cpu/docker/build.sh
@@ -1,5 +1,8 @@
+#!/bin/bash
+
 export UID=$(id -u $USER)
 export GID=$(id -g $USER)
+
 docker compose -f docker-compose.yml build \
     --build-arg UID=$UID \
     --build-arg GID=$GID
--- container/sobits_ws/cpu/docker/run.sh
+++ container/sobits_ws/cpu/docker/run.sh
@@ -1,1 +1,6 @@
+#!/bin/bash
+
+export UID=$(id -u) 
+export GID=$(id -g)
+
 docker compose -f docker-compose.yml up -d

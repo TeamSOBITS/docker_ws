7cf535a618f0e8e49910a3b2e252e90668f9b2b2
Suzuki Koshiro <ksuzuki9541@gmail.com>
2025-08-04 21:17:05 +0900

    [UPD] enable audio

--- container/sobits_ws/cpu/Dockerfiles/build.sh
+++
@@ -1,3 +0,0 @@
-export UID=$(id -u $USER)
-export GID=$(id -g $USER)
-docker compose -f docker-compose.yml build --build-arg UID=$UID --build-arg GID=$GID
---
+++ container/sobits_ws/gpu/docker/build.sh
@@ -0,0 +1,5 @@
+export UID=$(id -u $USER)
+export GID=$(id -g $USER)
+docker compose -f docker-compose.yml build \
+    --build-arg UID=$UID \
+    --build-arg GID=$GID

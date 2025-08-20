7eab775a8a043156f012ff92def2a897f9fd6327
Suzuki Koshiro <ksuzuki9541@gmail.com>
2025-08-20 22:42:02 +0900

    [UPD] generate .env file

--- container/sobits_ws/docker/build.sh
+++ container/sobits_ws/docker/build.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
 
 # Load environment variables
-export $(cat env.sh | grep -v '^#' | xargs)
+if [[ -f "./env.sh" ]]; then
+  source ./env.sh
+else
+  echo "env.sh not found"; exit 1
+fi
+
+# Generate .env file
+cat > .env <<EOF
+LOCAL_UID=${LOCAL_UID}
+LOCAL_GID=${LOCAL_GID}
+COMPUTE_TYPE=${COMPUTE_TYPE}
+USER_NAME=${USER_NAME}
+WORKSPACE_NAME=${WORKSPACE_NAME}
+UBUNTU_VERSION=${UBUNTU_VERSION}
+INSTALL_ROS=${INSTALL_ROS}
+ROS_DISTRO=${ROS_DISTRO}
+CUDA_VERSION=${CUDA_VERSION}
+EOF
+echo "Generated .env:"
 
 # Check COMPUTE_TYPE and validate
 if [ -z "${COMPUTE_TYPE}" ]; then

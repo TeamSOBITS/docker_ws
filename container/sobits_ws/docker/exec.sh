f73a5cc0188b42c3af9ad0a07c42985a7f789e2a
Suzuki Koshiro <ksuzuki9541@gmail.com>
2025-08-20 22:42:50 +0900

    [UPD] load env.sh

--- container/sobits_ws/docker/exec.sh
+++ container/sobits_ws/docker/exec.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 # Load environment variables
-export $(cat env.sh | grep -v '^#' | xargs)
+if [[ -f "./env.sh" ]]; then
+  source ./env.sh
+else
+  echo "env.sh not found"; exit 1
+fi
 
 # Check COMPUTE_TYPE
 if [ -z "${COMPUTE_TYPE}" ]; then

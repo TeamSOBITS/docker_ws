a08da9100c486f4589640894cc94e0aed27eddce
MrKeith99 <kvalentincardenas@hotmail.com>
2026-05-05 22:23:22 +0900

    [FIX] do not create ros_entrypoint if not required

--- container/sobits_ws/docker/build.sh
+++ container/sobits_ws/docker/build.sh
@@ -87,24 +87,6 @@ ROS_BASE_STAGE=${ROS_BASE_STAGE}
 FINAL_STAGE=${FINAL_STAGE}
 EOF
 
-if [[ "${INSTALL_ROS}" == "true" ]]; then
-    if [[ "${ROS_DISTRO}" == "noetic" ]]; then
-cat > ros_entrypoint.sh <<EOF
-source /opt/ros/${ROS_DISTRO}/setup.bash
-source ~/${ROS_WORKSPACE}/devel/setup.bash
-export ROS_MASTER_URI=http://localhost:11311
-alias cm='CURRENT_DIR=\`pwd\` && cd ~/${ROS_WORKSPACE}/ && catkin_make && source ~/.bashrc && cd \${CURRENT_DIR}'
-EOF
-    else
-cat > ros_entrypoint.sh <<EOF
-source /opt/ros/${ROS_DISTRO}/setup.bash
-source ~/${ROS_WORKSPACE}/install/setup.bash
-source /usr/share/colcon_argcomplete/hook/colcon-argcomplete.bash
-export ROS_DOMAIN_ID=${ROS_DOMAIN_ID}
-alias cb='CURRENT_DIR=\`pwd\` && cd ~/${ROS_WORKSPACE}/ && colcon build --symlink-install && source ~/.bashrc && cd \${CURRENT_DIR}'
-EOF
-    fi
-fi
 # =============================================================================
 # Main Command Logic
 # =============================================================================
@@ -193,6 +175,25 @@ case ${COMMAND} in
       echo "Using pre-built ROS image: ${ROS_IMAGE_TAG}"
     fi
     echo ""
+
+    if [[ "${INSTALL_ROS}" == "true" ]]; then
+        if [[ "${ROS_DISTRO}" == "noetic" ]]; then
+cat > ros_entrypoint.sh <<EOF
+source /opt/ros/${ROS_DISTRO}/setup.bash
+source ~/${ROS_WORKSPACE}/devel/setup.bash
+export ROS_MASTER_URI=http://localhost:11311
+alias cm='CURRENT_DIR=\`pwd\` && cd ~/${ROS_WORKSPACE}/ && catkin_make && source ~/.bashrc && cd \${CURRENT_DIR}'
+EOF
+        else
+cat > ros_entrypoint.sh <<EOF
+source /opt/ros/${ROS_DISTRO}/setup.bash
+source ~/${ROS_WORKSPACE}/install/setup.bash
+source /usr/share/colcon_argcomplete/hook/colcon-argcomplete.bash
+export ROS_DOMAIN_ID=${ROS_DOMAIN_ID}
+alias cb='CURRENT_DIR=\`pwd\` && cd ~/${ROS_WORKSPACE}/ && colcon build --symlink-install && source ~/.bashrc && cd \${CURRENT_DIR}'
+EOF
+        fi
+    fi
 
     if [ "${COMPUTE_TYPE}" = "gpu" ]; then
         if ! command -v nvidia-smi &> /dev/null; then
